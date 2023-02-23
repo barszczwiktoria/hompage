42964b197d003241d1c37d0d78640e32f1da10a5
barszczwiktoria <barszcz.wiktoria@gmail.com>
2023-02-23 16:32:35 +0100

    ES6+ features added

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
-console.log("Hello world!")
+{
+    const welcome = () => {
+        console.log("Hello world!")
+    }
+    
+    
 
-let button = document.querySelector(".section__button");
-let table = document.querySelector(".table");
-let button2 = document.querySelector(".js-scrollUp");
-let changeBackground = document.querySelector(".js-changeBackground");
-let body = document.body;
-let changeColor = document.querySelector(".js-changeColor");
-
-
-button.addEventListener("click", function handleClick() {
-    button.textContent = "taka mała zmiana :) ";
-});
-
-button.addEventListener("click", () => {
-    table.remove();
-});
-
-button2.addEventListener("click", () => {
-    scroll(0, 0);
-});
-
-
-changeBackground.addEventListener("click", () => {
-    body.classList.toggle("body--white");
-    changeColor.innerText = body.classList.contains("body--white") ? "szare" : "białe"
-});
+    const button = document.querySelector(".section__button");
+    const table = document.querySelector(".table");
+    const button2 = document.querySelector(".js-scrollUp");
+    const changeBackground = document.querySelector(".js-changeBackground");
+    const body = document.body;
+    const changeColor = document.querySelector(".js-changeColor");
+    
+    
+    button.addEventListener("click", function handleClick() {
+        button.textContent = "taka mała zmiana :) ";
+    });
+    
+    button.addEventListener("click", () => {
+        table.remove();
+    });
+    
+    button2.addEventListener("click", () => {
+        scroll(0, 0);
+    });
+    
+   welcome ();
+}
