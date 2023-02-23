9a3b12f3160df6a9f37f09abf604d9c0ddec3508
barszczwiktoria <barszcz.wiktoria@gmail.com>
2023-02-23 17:56:48 +0100

    ES6+ features update

--- js/script.js
+++ js/script.js
@@ -3,27 +3,45 @@
         console.log("Hello world!")
     }
     
-    
 
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const changeColor = document.querySelector(".js-changeColor");
+
+        body.classList.toggle("body--white");
+        changeColor.innerText = body.classList.contains("body--white") ? "szare" : "białe";
+    };
+
+
+const removeTable = () => {
     const button = document.querySelector(".section__button");
     const table = document.querySelector(".table");
+
+    button.textContent = "taka mała zmiana :) ";
+    table.remove; //ta opcja nie chce mi zadziałać, nie jestem pewna co tu powinnam dodać. 
+}
+    
+const scrollUp = () => {
     const button2 = document.querySelector(".js-scrollUp");
-    const changeBackground = document.querySelector(".js-changeBackground");
-    const body = document.body;
-    const changeColor = document.querySelector(".js-changeColor");
-    
-    
-    button.addEventListener("click", function handleClick() {
-        button.textContent = "taka mała zmiana :) ";
-    });
-    
-    button.addEventListener("click", () => {
-        table.remove();
-    });
-    
     button2.addEventListener("click", () => {
         scroll(0, 0);
-    });
+    }); // czy funkcja strzałkowa moze byc uzyta w funkcji strzałkowej? nie mam innego pomysłu jak to zapisać :(
+}
     
-   welcome ();
+
+    
+    const init = () => {
+      const changeBacground = document.querySelector(".js-changeBackground");
+        changeBacground.addEventListener("click", toggleBackground);
+
+        const button = document.querySelector(".section__button");
+        button.addEventListener("click", removeTable);
+
+        const button2 = document.querySelector(".js-scrollUp");
+        button2.addEventListener("click", scrollUp);
+
+        welcome ();
+    }
+   
+    init ()
 }
